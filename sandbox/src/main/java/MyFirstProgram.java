4445e9a0d994267d3d1bbeda9449fa34430011bf
Artem Rudenko <artemr@compatibl.com>
2016-11-17 17:28:29 +0300

    Update main()

--- sandbox/src/main/java/MyFirstProgram.java
+++ sandbox/src/main/java/MyFirstProgram.java
@@ -1,6 +1,11 @@
 public class MyFirstProgram{
     public static void main(String args[]){
-        System.out.println("Hello World!");
+        System.out.println("Started....");
+        for(int i=0;i<10;i++){
+            System.out.println("It's alive!!! Attempt: " + i);
+        }
+        System.out.println("Stopped!");
+
     }
     
 }
