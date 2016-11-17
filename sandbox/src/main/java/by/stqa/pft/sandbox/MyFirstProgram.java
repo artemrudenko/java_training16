e6e3c644a36edacac4ef5889c31b0d7c56492b0e
Artem Rudenko <artemr@compatibl.com>
2016-11-17 19:27:09 +0300

    Point implementation changed

--- sandbox/src/main/java/by/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/by/stqa/pft/sandbox/MyFirstProgram.java
@@ -3,22 +3,24 @@ package by.stqa.pft.sandbox;
 public class MyFirstProgram {
   public static void main(String args[]) {
     System.out.println("Started....");
+
     for (int i = 0; i < 2; i++) {
       System.out.println("It's alive!!! Attempt: " + i);
     }
     System.out.println("Stopped!");
     Square sq = new Square(5);
-    System.out.println("Square area is: " + area(sq) + " Side len is: " + sq.l);
+    System.out.println("Square area is: " + sq.area() + " Side len is: " + sq.l);
     Rectangle rc = new Rectangle(3, 5);
-    System.out.println("Rect with a = " + rc.a + " and b = " + rc.b + " has area = " + area(rc));
+    System.out.println("Rect with a = " + rc.a + " and b = " + rc.b + " has area = " + rc.area());
+
+    Point pA = new Point(1, 3);
+    Point pB = new Point(2, 12);
+    System.out.println("a) Distance with function is: " + distance(pA, pB));
+    System.out.println("b) Distance with method is: " + pA.distance(pB));
 
   }
 
-  public static double area(Square s){
-    return s.l * s.l;
+  public static double distance(Point p1, Point p2){
+    return Math.sqrt(Math.pow(p2.x - p1.x, 2) + Math.pow(p2.y - p1.y, 2));
   }
-  public static double area(Rectangle r){
-    return r.a * r.b;
-  }
-
 }
--- sandbox/src/main/java/by/stqa/pft/sandbox/Rectangle.java
+++ sandbox/src/main/java/by/stqa/pft/sandbox/Rectangle.java
@@ -12,4 +12,7 @@ public class Rectangle {
     this.a = a;
     this.b = b;
   }
+  public double area(){
+    return this.a * this.b;
+  }
 }
