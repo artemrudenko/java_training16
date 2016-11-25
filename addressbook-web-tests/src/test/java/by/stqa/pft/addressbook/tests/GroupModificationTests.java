20ab3adbc5a45fa3dfad09d05dfad4f4b7f68ae5
Artem Rudenko <artemr@compatibl.com>
2016-11-25 13:19:09 +0300

    group modification test added

---
+++ addressbook-web-tests/src/test/java/by/stqa/pft/addressbook/tests/GroupModificationTests.java
@@ -0,0 +1,20 @@
+/**
+ * Created by artemr on 11/25/2016.
+ */
+package by.stqa.pft.addressbook.tests;
+
+import by.stqa.pft.addressbook.model.GroupData;
+import org.testng.annotations.Test;
+
+public class GroupModificationTests extends TestBase {
+  @Test
+  public void testGroupModification(){
+    app.getNavigationHelper().gotoGroupPage();
+    app.getGroupHelper().selectGroup();
+    app.getGroupHelper().initGroupModification();
+    app.getGroupHelper().fillGroupForm(new GroupData("NAMEE", "HEADER", "FOOTER"));
+    app.getGroupHelper().submitGroupModification();
+    app.getGroupHelper().returnToGroupPage();
+  }
+
+}
