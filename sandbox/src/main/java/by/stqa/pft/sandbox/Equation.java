a3235f0b994067723ef0bc0d831784e5cad46ee2
Artem Rudenko <artemr@compatibl.com>
2016-12-01 12:58:19 +0300

    equatation added

---
+++ sandbox/src/main/java/by/stqa/pft/sandbox/Equation.java
@@ -0,0 +1,27 @@
+package by.stqa.pft.sandbox;
+
+/**
+ * Created by artemr on 12/1/2016.
+ */
+public class Equation {
+  private double a;
+  private double b;
+  private double c;
+  private int n;
+
+  public Equation(double a, double b, double c){
+    this.a = a;
+    this.b = b;
+    this.c = c;
+
+    double d = b*b - 4*a*c;
+    if(d > 0){
+      n = 2;
+    }else {
+      n = (d == 0)? 1: 0;
+    }
+  }
+  public int rootNumber(){
+    return n;
+  }
+}
