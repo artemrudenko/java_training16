1214ca202cc3894797bc03f9f52175c901966754
Artem Rudenko <artemr@compatibl.com>
2016-12-01 13:12:55 +0300

    more checks added

--- sandbox/src/main/java/by/stqa/pft/sandbox/Equation.java
+++ sandbox/src/main/java/by/stqa/pft/sandbox/Equation.java
@@ -9,19 +9,24 @@ public class Equation {
   private double c;
   private int n;
 
-  public Equation(double a, double b, double c){
+  public Equation(double a, double b, double c) {
     this.a = a;
     this.b = b;
     this.c = c;
 
-    double d = b*b - 4*a*c;
-    if(d > 0){
-      n = 2;
-    }else {
-      n = (d == 0)? 1: 0;
+    double d = b * b - 4 * a * c;
+    if (a != 0) {
+      n = (d > 0) ? 2 : (d == 0) ? 1 : 0;
+    } else if (b != 0) {
+      n = 1;
+    } else if (c != 0) {
+      n = 0;
+    } else {
+      n = -1;
     }
   }
-  public int rootNumber(){
+
+  public int rootNumber() {
     return n;
   }
 }
