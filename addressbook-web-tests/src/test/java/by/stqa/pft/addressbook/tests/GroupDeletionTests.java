b1869d2e2a7a77256ab654ad9fd495adf023c09c
Artem Rudenko <artemr@compatibl.com>
2016-12-01 16:00:31 +0300

    group exist check added

--- addressbook-web-tests/src/test/java/by/stqa/pft/addressbook/tests/GroupDeletionTests.java
+++ addressbook-web-tests/src/test/java/by/stqa/pft/addressbook/tests/GroupDeletionTests.java
@@ -4,6 +4,7 @@
 
 package by.stqa.pft.addressbook.tests;
 
+import by.stqa.pft.addressbook.model.GroupData;
 import org.testng.annotations.Test;
 
 public class GroupDeletionTests extends TestBase {
@@ -11,6 +12,9 @@ public class GroupDeletionTests extends TestBase {
   @Test
   public void testGroupDeletion() {
     app.getNavigationHelper().gotoGroupPage();
+    if(!app.getGroupHelper().isThereAGroup()){
+      app.getGroupHelper().createGroup(new GroupData("MyGroup1", null, null));
+    }
     app.getGroupHelper().selectGroup();
     app.getGroupHelper().deleteSelectedGroups();
     app.getGroupHelper().returnToGroupPage();
--- addressbook-web-tests/src/test/java/by/stqa/pft/addressbook/tests/GroupModificationTests.java
+++ addressbook-web-tests/src/test/java/by/stqa/pft/addressbook/tests/GroupModificationTests.java
@@ -10,6 +10,10 @@ public class GroupModificationTests extends TestBase {
   @Test
   public void testGroupModification(){
     app.getNavigationHelper().gotoGroupPage();
+    if(!app.getGroupHelper().isThereAGroup()){
+      app.getGroupHelper().createGroup(new GroupData("MyGroup1", null, null));
+    }
+
     app.getGroupHelper().selectGroup();
     app.getGroupHelper().initGroupModification();
     app.getGroupHelper().fillGroupForm(new GroupData("NAMEE", "HEADER", "FOOTER"));
