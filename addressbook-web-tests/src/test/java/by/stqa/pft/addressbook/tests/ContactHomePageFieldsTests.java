792a79fe7e9ceef4a41a05517ca4c54babe23874
artemr <artemr@compatibl.com>
2016-12-20 10:36:03 +0300

    single class with tests for home page fields(email, addr and phones) added

---
+++ addressbook-web-tests/src/test/java/by/stqa/pft/addressbook/tests/ContactHomePageFieldsTests.java
@@ -0,0 +1,63 @@
+package by.stqa.pft.addressbook.tests;
+
+import by.stqa.pft.addressbook.model.ContactData;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+/**
+ * Created by artemr on 12/20/2016.
+ */
+public class ContactHomePageFieldsTests extends TestBase{
+  @BeforeMethod
+  public void ensurePreconditions() {
+    app.goTo().homePage();
+    if(app.contact().all().size() == 0){
+      app.contact().create(app.contact().generate());
+    }
+  }
+
+  @Test
+  public void testContactAddress(){
+    ContactData contact = app.contact().all().iterator().next();
+    ContactData contactInfoFromEditForm = app.contact().infoFromEditForm(contact);
+    assertThat(contact.getAddress(), equalTo(contactInfoFromEditForm.getAddress()));
+  }
+
+  @Test
+  public void testContactEmails(){
+    ContactData contact = app.contact().all().iterator().next();
+    ContactData contactInfoFromEditForm = app.contact().infoFromEditForm(contact);
+    assertThat(contact.getAllEmails(), equalTo(mergeEmails(contactInfoFromEditForm)));
+  }
+
+  @Test
+  public void testContactPhones(){
+    ContactData contact = app.contact().all().iterator().next();
+    ContactData contactInfoFromEditForm = app.contact().infoFromEditForm(contact);
+    assertThat(contact.getAllPhones(), equalTo(mergePhones(contactInfoFromEditForm)));
+  }
+
+  private String mergePhones(ContactData contact) {
+    return Stream.of(contact.getHomePhone(), contact.getMobilePhone(), contact.getWorkPhone())
+            .filter((s)-> ! s.equals(""))
+            .map(ContactHomePageFieldsTests::cleaned)
+            .collect(Collectors.joining("\n"));
+  }
+
+  public static String cleaned(String phone){
+    return phone.replaceAll("\\s", "").replaceAll("[-()]", "");
+  }
+
+  private String mergeEmails(ContactData contact) {
+    return Stream.of(contact.getEmail(), contact.getEmail2(), contact.getEmail3())
+            .filter((s)-> ! s.equals(""))
+            .collect(Collectors.joining("\n"));
+  }
+
+}
