7a5c191906ccc8009966565732a97ec8be0f1b76
artemr <artemr@compatibl.com>
2016-12-29 11:10:24 +0300

    map ContactData to ORM

--- addressbook-web-tests/src/test/java/by/stqa/pft/addressbook/model/ContactData.java
+++ addressbook-web-tests/src/test/java/by/stqa/pft/addressbook/model/ContactData.java
@@ -7,53 +7,91 @@ package by.stqa.pft.addressbook.model;
 import com.google.gson.annotations.Expose;
 import com.thoughtworks.xstream.annotations.XStreamAlias;
 import com.thoughtworks.xstream.annotations.XStreamOmitField;
+import org.hibernate.annotations.Type;
 
+import javax.persistence.*;
 import java.io.File;
 
 @XStreamAlias("contact")
+@Entity
+@Table(name="addressbook")
 public class ContactData {
   @XStreamOmitField
+  @Id
+  @Column(name="id")
   private int id = Integer.MAX_VALUE;
   @Expose
+  @Column(name="firstname")
   private String firstname;
+  @Transient
   private String middlename;
   @Expose
+  @Column(name="lastname")
   private String lastname;
+  @Transient
   private String nickname;
+  @Transient
   private String title;
+  @Transient
   private String company;
   @Expose
+  @Column(name="address")
+  @Type(type="text")
   private String address;
   @Expose
+  @Column(name="home")
+  @Type(type="text")
   private String homePhone;
   @Expose
+  @Column(name="mobile")
+  @Type(type="text")
   private String mobilePhone;
   @Expose
+  @Column(name="work")
+  @Type(type="text")
   private String workPhone;
+  @Transient
   private String fax;
   @Expose
+  @Column(name="email")
+  @Type(type="text")
   private String email;
   @Expose
+  @Column(name="email2")
+  @Type(type="text")
   private String email2;
   @Expose
+  @Column(name="email3")
+  @Type(type="text")
   private String email3;
+  @Transient
   private String homepage;
+  @Transient
   private String birthday;
+  @Transient
   private String anniversary;
+  @Transient
   private String group;
+  @Transient
   private String address2;
+  @Transient
   private String phone2;
+  @Transient
   private String notes;
+  @Transient
   private String allPhones;
+  @Transient
   private String allEmails;
-  private File photo;
+  @Column(name="photo")
+  @Type(type="text")
+  private String photo;
 
   public File getPhoto() {
-    return photo;
+    return new File(photo);
   }
 
   public ContactData withPhoto(File photo) {
-    this.photo = photo;
+    this.photo = photo.getPath();
     return this;
   }
 
@@ -291,7 +329,6 @@ public class ContactData {
             "id=" + id +
             ", firstname='" + firstname + '\'' +
             ", lastname='" + lastname + '\'' +
-            ", address='" + address + '\'' +
             '}';
   }
 
